ffbb4aa14de398b21f2df666cbae62ac09504a9a
atmar kohistany <atmar@hotmail.dk>
2020-04-13 23:43:47 +0200

    Request_Loan Xml done

--- app/src/main/java/com/example/loanandrepay/RequestLoan.java
+++ app/src/main/java/com/example/loanandrepay/RequestLoan.java
@@ -10,6 +10,10 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -18,13 +22,41 @@ import java.util.Objects;
 public class RequestLoan extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
+    private Spinner spinner1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_loan);
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
+    }
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) new CustomOnItemSelectedListener());
     }
 
-    //Here the logout button is hidden, when the user is logged out
+    // get the selected dropdown list value
+    public void addListenerOnButton() {
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+
+    }
+
+
+    public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            Toast.makeText(parent.getContext(),
+                    "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // TODO Auto-generated method stub
+        }
+    }
+//    //Here the logout button is hidden, when the user is logged out
     @Override
     protected void onStart() {
         super.onStart();
